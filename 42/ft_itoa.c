0906291a00c89b9c309c2dcc000e02a58c1a13c4
kuzi23 <kwizeramariustresor39@gmail.com>
2024-03-05 20:27:08 +0100

    lib2

--- 42/ft_itoa.c
+++ 42/ft_itoa.c
@@ -6,11 +6,56 @@
 /*   By: mkwizera <mkwizera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:21:36 by mkwizera          #+#    #+#             */
-/*   Updated: 2024/03/05 15:23:02 by mkwizera         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:26:35 by mkwizera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_itoa(int n)
+#include "libft.h"
+
+int	ft_countsize(int n)
 {
-    
+	int	size;
+	int	num;
+
+	size = 0;
+	num = n;
+	if (num == 0)
+		return (1);
+	while (num > 0)
+	{
+		num /= 10;
+		size++;
+	}
+	return (size);
 }
+
+
+char	*ft_itoa(int num)
+{
+	char	*str;
+	int		digit_count;
+
+	digit_count = 0;
+	if (num == 0)
+		return (ft_strdup("0"));
+	if (num == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (num < 0)
+	{
+		num = -1 * num;
+		digit_count++;
+	}
+	digit_count += count_digits(num);
+	if (!(str == (char *)malloc(sizeof(char) * (digit_count + 1))))
+		return (NULL);
+	str[digit_count] = '\0';
+	while (num > 0)
+	{
+		str[digit_count-- - 1] = num % 10 + '0';
+		num = num / 10;
+	}
+	if (digit_count == 1)
+		str[0] = '-';
+	return (str);
+}
+
--- 42/ft_strdup.c
+++ 42/ft_strdup.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kuzi <kuzi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mkwizera <mkwizera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:57:08 by mkwizera          #+#    #+#             */
-/*   Updated: 2024/03/03 09:58:53 by kuzi             ###   ########.fr       */
+/*   Updated: 2024/03/05 16:55:20 by mkwizera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-    size_t size;
-    char *dest;
-    size_t i;
+	size_t	size;
+	char	*dest;
+	size_t	i;
 
-    size = ft_strlen(s1);
-    dest = (char *)malloc(size + 1);
-    if (dest == NULL)
-    {
-        return (NULL);
-    }
-    i = 0;
-    while (s1[i] != '\0' && i < size)
-    {
-        dest[i] = s1[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	size = ft_strlen(s1);
+	dest = (char *)malloc(size + 1);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0' && i < size)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
