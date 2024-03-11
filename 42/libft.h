92ddf64bde225ef3755e049745d64bea25c7cfbe
kuzi23 <kwizeramariustresor39@gmail.com>
2024-03-11 11:33:45 +0100

    good

--- 42/libft.h
+++ 42/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkwizera <mkwizera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:18:30 by mkwizera          #+#    #+#             */
-/*   Updated: 2024/03/11 11:22:31 by mkwizera         ###   ########.fr       */
+/*   Updated: 2024/03/11 11:32:31 by mkwizera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 size_t		ft_strlen(const char *str);
 int			ft_atoi(char *str);
 int			ft_strncmp(char *s1, char *s2, unsigned int n);
-int			ft_isadigit(int dg);
+int			ft_isdigit(int dg);
 int			ft_isalnum(int n);
 int			ft_isalpha(int ch);
 int			ft_isascii(int c);
@@ -44,11 +44,10 @@ char		*ft_itoa(int n); // need a review .
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strchr(const char *str, int c);
-int			ft_countsize(int n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 char		*ft_strtrim(char const *s1, char const *set);
 void		*ft_memmove(void *dst, const void *src, size_t len);//review
-int			ft_tolower(int z);
+int			ft_tolower(int c);
 char		**ft_split(char const *s, char c);
 int			ft_strlcpy(char *dest, char *src, int n);
